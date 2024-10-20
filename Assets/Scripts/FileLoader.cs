c282e4ce04f62496629aff521fda58bfbdeb07e8
Patchouli <extrasakuya@outlook.com>
2024-10-20 14:36:45 +0800

    Initial commit

--- Assets/Scripts/FileLoader.cs
+++ Assets/Scripts/FileLoader.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class FileLoader : MonoBehaviour
 {
     public string directoryPath = "";
+    public Text jsonContentText;
     public UIManager uiManager;
 
     void Start()
@@ -19,5 +21,13 @@ public class FileLoader : MonoBehaviour
             Debug.LogError("UIManager reference is not set.");
         }
     }
-
+    public void ReLoad()
+    {
+        foreach (Transform child in uiManager.fileListContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        Start();
+        jsonContentText.text = "";
+    }
 }
